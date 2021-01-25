7c71516d5bb799a679b1b7d436b31f2a6fa7101e
Posampas <piotr.mierzejewski@mail.com>
2021-01-25 15:46:20 +0100

    Response Entity added

--- src/main/java/com/posampas/bar/controller/Controller.java
+++ src/main/java/com/posampas/bar/controller/Controller.java
@@ -3,14 +3,19 @@ package com.posampas.bar.controller;
 import com.posampas.bar.dto.InvoiceDto;
 import com.posampas.bar.dto.PlaceOrderDto;
 import com.posampas.bar.dto.PrintBillDto;
+import com.posampas.bar.dvo.Invoice;
+import com.posampas.bar.dvo.Recipe;
 import com.posampas.bar.entities.BeverageEntity;
 import com.posampas.bar.facade.BarFacade;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -21,25 +26,26 @@ public class Controller {
 
 
     @GetMapping("/menu")
-    public Collection<BeverageEntity> getMenu(){
-            return facade.getMenu();
+    public ResponseEntity<Collection<BeverageEntity>>getMenu(){
+            Collection<BeverageEntity> list = facade.getMenu();
 
+            return  new ResponseEntity<>(list, HttpStatus.OK);
     }
 
     @PostMapping("/order")
-    public UUID placeOrder(@RequestBody PlaceOrderDto dto){
-         return facade.placeAnOrder(dto);
+    public ResponseEntity<UUID> placeOrder(@RequestBody PlaceOrderDto dto){
+         return new ResponseEntity<>( facade.placeAnOrder(dto), HttpStatus.OK);
 
     }
 
     @GetMapping("/recipePrint")
-    public String getBill(@RequestBody PrintBillDto dto){
-        return facade.printRecipe(dto.getBillID()).toString();
+    public ResponseEntity<Recipe> getBill(@RequestBody PrintBillDto dto){
+        return new ResponseEntity<>(facade.printRecipe(dto.getBillID()),HttpStatus.OK);
     }
 
     @GetMapping("/invoice")
-    public String getInvoice(@RequestBody InvoiceDto dto){
-        return facade.printInvoice(dto).toString();
+    public ResponseEntity<Invoice> getInvoice(@RequestBody InvoiceDto dto){
+        return new ResponseEntity<>(facade.printInvoice(dto),HttpStatus.OK);
     }
 
 
